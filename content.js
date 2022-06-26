20b810ff5f36e01fcf68234f74d1317a4d099bef
towakey <kyosgame@gmail.com>
2022-06-27 00:49:28 +0900

    拡張機能として最低限の準備

---
+++ content.js
@@ -0,0 +1,8 @@
+console.log("run...");
+// let pclass=document.querySelectorAll("p");
+let imgelement=document.images;
+for(let i=0;i<imgelement.length;i++){
+    let e=imgelement[i];
+    e.style.display='none';
+}
+console.log("...finish");
